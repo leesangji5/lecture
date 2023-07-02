22eed15bdd7c4346ff69acd8922007bdf61b1902
sangji <lsgsh211@gmail.com>
2023-07-03 07:06:10 +0900

    add .

---
+++ lecture.py
@@ -0,0 +1,82 @@
+'''
+python version 3.11.3
+
+
+curriculum
+
+1. print
+2. variables
+3. input
+4. if
+5. for
+6. while
+7. function
+7.1 basic of function
+7.2 parameter
+7.3 return
+7.4 add, sub, mul, div
+8. class
+8.1 basic of class
+8.2 class variable
+8.3 class function
+8.4 class isntance
+8.5 class constructor, destructor
+8.6 class inheritance
+9. library
+10. module
+11. file
+12. exception
+13. database
+14. project
+
+project: 
+
+
+7/3
+1. print
+2. variables
+3. input
+4. if
+
+7/4
+5. for
+6. while
+
+7/5
+7. function
+7.1 basic of function
+
+7/6
+7.2 parameter
+7.3 return
+
+7/7
+7.4 add, sub, mul, div
+
+7/10
+8. class
+8.1 basic of class
+8.2 class variable
+8.3 class function
+
+7/11
+8.4 class isntance
+8.5 class constructor, destructor
+8.6 class inheritance
+
+7/12
+9. library
+10. module
+
+7/13
+11. file
+12. exception
+
+7/14
+13. database
+
+7/15
+14. project
+
+
+'''
--- lecture1.py
+++ lecture1.py
@@ -1,15 +1,9 @@
-# lectuer 1
-# python version 3.11.3
-# 6/29/2023
-
 # print
-
 print("Hello World!")
 print(1234567890)
 print(3.1415)
 
 # variables
-
 string = "Hello World!"
 integer = 1234567890
 floatnum = 3.1415
@@ -17,11 +11,9 @@ floatnum = 3.1415
 print(string)
 print(integer)
 print(floatnum)
-
 print(string, integer, floatnum)
 
 # input
-
 name = input("your name?")
 age = input("your age?")
 phone = input("your phone number?")
@@ -29,3 +21,27 @@ phone = input("your phone number?")
 print("Your name: " + name)
 print("You age: " + age)
 print("Your phone numbe: " + phone)
+
+# if elif else
+name = input("your name?")
+age = input("your age?")
+phone = input("your phone number?")
+
+if name == "sangji":
+    print("Hello, Sangji")
+elif name == "hyongmin":
+    print("Hello, Hyongmin")
+else:
+    print("Sorry, I dont know")
+
+myAge = 19
+if myAge < age:
+    print("older than me")
+elif myAge > age:
+    print("younger than me")
+else:
+    print("same age")
+
+myPhone = "010-1234-5678"
+if myPhone == phone:
+    print("impossible")
