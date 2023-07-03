0ad33f2139203a79268e648432a47c25ae05794d
sangji <lsgsh211@gmail.com>
2023-07-03 14:37:21 +0900

    add .

---
+++ lecture5.py
@@ -0,0 +1,19 @@
+# library
+import time
+
+print("Hello World!")
+time.sleep(1)
+print("Hello World!")
+
+import random
+num = random.randint(1, 10)
+print(num)
+
+while True:
+    print(random.randint(1, 10))
+
+# module
+import myModule
+
+print(myModule.sum()
+)
