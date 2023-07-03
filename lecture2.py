c8414b59845dbf55a234038159382bd0084eb287
sangji <lsgsh211@gmail.com>
2023-07-03 11:27:33 +0900

    add .

--- lecture2.py
+++ lecture2.py
@@ -1,1 +1,47 @@
 # for
+for  i in range(5):
+    print(i)
+
+for i in range(0, 5, 2):
+    print(i)
+
+array = [1, 2, 3, 4, 5]
+print(array[0])
+print(array[1])
+print(array[2])
+print(array[3])
+print(array[4])
+
+for i in range(len(array)):
+    print(array[i])
+
+customers = ["sangji", 18, "010-1234-5678"]
+for i in range(len(customers)):
+    print(customers[i])
+
+# while
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+# while True:
+#     print(i)
+#     i += 1
+
+while True:
+    print(i)
+    i += 1
+    if i == 5:
+        break
+
+while True:
+    answer = input("your name?")
+    if answer == "sangji":
+        print("Hello, Sangji")
+        break
+    elif answer == "hyongmin":
+        print("Hello, Hyongmin")
+        break
+    else:
+        print("Sorry, I dont know")
---
+++ lecture3.py
@@ -0,0 +1,33 @@
+# function
+# 8.1 basic of function
+
+def printHello():
+    print("Hello World!")
+
+printHello()
+
+def main():
+    printHello()
+
+main()
+
+# 7.2 parameter
+def printName(name):
+    print("Hello, " + name)
+
+printName("sangji")
+
+# 7.3 return
+def returnName():
+    return "sangji"
+
+name = returnName()
+print(name)
+
+
+def printName(name):
+    string = "Hello, " + name
+    return string
+
+name = printName("sangji")
+print(name)
