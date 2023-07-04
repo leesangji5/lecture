34a484d69cf02d6e0f9c20a00234ceb01dec6dd1
sangji <lsgsh211@gmail.com>
2023-07-04 14:29:53 +0900

    add .

---
+++ h1.py
@@ -0,0 +1,76 @@
+"""
+개념 정리
+print, input, 변수, 조건문, if, elif, else를 설명하시오.
+"""
+
+"""
+난이도 ★☆☆☆☆☆☆☆☆☆
+이름, 나이, 전화번호를 입력 받는다.
+
+이름, 나이, 전화번호를 한번에 출력해라
+
+사용 가능한 것들
+input 3개, print 1개 변수 3개
+print를 하나만 사용해라
+"""
+
+"""
+난이도 ★✬☆☆☆☆☆☆☆☆
+이름을 입력 받는다.
+만약 이름이 sangji라면 "Hello, Sangji"를 출력한다.
+만약 이름이 hyongmin이라면 "Hello, Hyongmin"를 출력한다.
+모르는 이름이라면 "Sorry, I dont know"를 출력한다.
+조건문을 활용하시오.
+"""
+
+"""
+난이도 ★★✬☆☆☆☆☆☆☆
+오늘 내용을 복습해보자.
+코드를 읽고 코드가 무엇을 하는지 설명하시오.
+
+name = input("your name?")
+age = input("your age?")
+phone = input("your phone number?")
+
+if name == "sangji":
+    print("Hello, Sangji")
+elif name == "hyongmin":
+    print("Hello, Hyongmin")
+else:
+    print("Sorry, I dont know")
+
+if myAge < age:
+    print("older than me")
+elif myAge > age:
+    print("younger than me")
+else:
+    print("same age")
+
+myPhone = "010-1234-5678"
+if myPhone == phone:
+    print("impossible")
+"""
+
+"""
+난이도 ★★★☆☆☆☆☆☆☆
+이름과 나이를 입력 받는다.
+만약 이름이 sangji이고 나이가 19라면 "Hello, Sangji"를 출력한다.
+만약 이름은 같지만 나이가 다르다면 "내 친구와 이름이 같아 "를 출력한다.
+만약 이름은 다르지만 나이가 같다면 "내 친구와 나이가 같아"를 출력한다.
+이름도 나이도 다르다면 "처음 뵙겠습니다."를 출력한다.
+조건문 활용
+and, or 을 참고하시오
+
+조건 and 조건 : 둘다 참일때만 참
+조건 or 조건 : 둘중 하나만 참이면 참
+ex) a = 1
+    b = 2
+    if a == 1 and b == 2:
+        print("a는 1이고 b는 2이다.")
+
+ex) a = 1
+    b = 2
+    if a == 1 or b == 3:
+        print("a는 1이거나 b는 3이다.")
+
+"""
